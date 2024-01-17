c392cfd4fd7c97465260c42bf9106a55a6919b42
SavioNicodemos <nicodemosgcosta@gmail.com>
2024-01-17 01:50:05 +0000

    Configure fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,35 @@
+/* eslint-disable global-require */
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { StatusBar, View } from 'react-native';
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 import AppProvider from './src/hooks';
 
 import Routes from './src/routes';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'RobotoSlab-Regular': require('./assets/fonts/RobotoSlab-Regular.ttf'),
+    'RobotoSlab-Medium': require('./assets/fonts/RobotoSlab-Medium.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <StatusBar
@@ -17,7 +38,10 @@ export default function App() {
         translucent
       />
       <AppProvider>
-        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <View
+          style={{ flex: 1, backgroundColor: '#312e38' }}
+          onLayout={onLayoutRootView}
+        >
           <Routes />
         </View>
       </AppProvider>
